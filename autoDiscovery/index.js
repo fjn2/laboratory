407d4e92f3d21040641779bc4b09a2e00d37dfb8
Federico Naveira <federiconaveira@gmail.com>
2017-09-22 21:29:04 -0300

    Initial commit

---
+++ autoDiscovery/index.js
@@ -0,0 +1,14 @@
+const UdpNode = require('udp-node')
+const six = new UdpNode()
+six
+  .set({
+    name: 'Six',
+    type: 'machine'
+  })
+  .broadcast()
+  .onNode((message, rinfo) => {
+    // FOUND NODE
+    // message: contains node's name, type and other details set when node was initialized using set()
+    // rinfo: contains node's ip address and port
+    console.log(message, rinfo);
+  })
